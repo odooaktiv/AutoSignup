104999187e1702f85711db0a115c1a8187d853f2
Burhan Vakharia <burhan@aktivsoftware.com>
2018-06-30 11:54:47 +0530

    [IMP] Fixed Pep8 syntax.

--- auto_signup/models/signup.py
+++ auto_signup/models/signup.py
@@ -2,13 +2,16 @@
 
 from openerp import models, api
 
+
 class SignupEnable(models.Model):
     _name = 'signup.enable'
 
     @api.model
     def _init_settings(self):
         base_settings_obj = self.env['base.config.settings']
-	#Activate allow signup with template user
-        base_settings_id = base_settings_obj.create({'auth_signup_uninvited':True, 'template_user_id': True})
-        base_settings_id.execute() #Execute setting
+        # Activate allow signup with template user
+        base_settings_id = base_settings_obj.create({
+            'auth_signup_uninvited': True,
+            'template_user_id': True})
+        base_settings_id.execute()  # Execute setting
         return True

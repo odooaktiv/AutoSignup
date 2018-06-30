97fbe75fe2516a93acc4acb95aa9c46a55f27819
Burhan Vakharia <burhan@aktivsoftware.com>
2018-06-30 12:11:29 +0530

    [IMP] Pep8 Fixes.

--- auto_signup/__manifest__.py
+++ auto_signup/__manifest__.py
@@ -6,8 +6,9 @@
         Automatic signup enable""",
 
     'description': """
-        No need to enable 'Allow external users to sign up' from General Settings
-	This module installation automatically enable external signup without any configuration with Template user
+        No need to enable 'Allow external users to sign up' from
+        General Settings. This module installation automatically
+        enable external signup without any configuration with Template user
     """,
 
     'author': "Aktiv software ",
@@ -19,9 +20,9 @@
     'depends': ['base_setup', 'website', 'portal'],
 
     'data': [
-	'views/signup_views.xml'
+        'views/signup_views.xml'
     ],
 
-    'images':  ['static/description/banner.jpg'],
+    'images': ['static/description/banner.jpg'],
     'application': True,
 }
